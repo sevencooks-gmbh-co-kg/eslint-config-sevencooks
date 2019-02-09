c32bcd0118ddaa78a3f7c0f4ece2fcc6f47af24e
Daniel Nehring <daniel.nehring@sevencooks.com>
2019-02-09 09:11:39 +0100

    feat(react): enable react hooks rules

--- react.js
+++ react.js
@@ -2,11 +2,15 @@
 /* eslint-disable sort-keys */
 
 module.exports = {
+  plugins: [
+    "react-hooks",
+  ],
   extends: [
     "standard-react",
     "prettier/react",
   ],
   rules: {
     /* eslint-enable sort-keys */
+    "react-hooks/rules-of-hooks": "error",
   },
 };
