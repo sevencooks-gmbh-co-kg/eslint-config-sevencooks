a8e274bc1bd01d8f3b6a077c258a5f99ee500ee3
Daniel Nehring <daniel.nehring@sevencooks.com>
2019-03-09 19:36:56 +0100

    feat(react): use more hooks rules

--- react.js
+++ react.js
@@ -2,15 +2,22 @@
 /* eslint-disable sort-keys */
 
 module.exports = {
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   plugins: [
     "react-hooks",
   ],
   extends: [
     "standard-react",
+    // This disables rules Prettier takes care of
     "prettier/react",
   ],
   rules: {
     /* eslint-enable sort-keys */
     "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "error",
   },
 };
